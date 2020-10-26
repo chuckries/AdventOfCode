ef3bfe10d5a60f0faf73a855ca2e7d957567c5dc
Chuck Ries <chuckr@microsoft.com>
2020-10-26 15:29:16 -0700

    more

--- AdventOfCode.2015/Day19.cs
+++ AdventOfCode.2015/Day19.cs
@@ -12,23 +12,23 @@ namespace AdventOfCode._2015
 {
     public class Day19
     {
-        private readonly ILookup<string, string> _lookup;
+        private readonly string[] _rawTransforms;
         private readonly string _initial;
 
         public Day19()
         {
             string[] input = File.ReadAllLines("Inputs/Day19.txt");
-            _lookup = input[..^2]
-                .Select(s => s.Split(' '))
-                .ToLookup(t => t[0], t => t[2]);
-
+            _rawTransforms = input[..^2];
             _initial = input[^1];
         }
 
         [Fact]
         public void Part1()
         {
-            HashSet<string> answers = new HashSet<string>(ComposeAll(_initial, _lookup));
+            ILookup<string, string> lookup = _rawTransforms
+                .Select(s => s.Split(' '))
+                .ToLookup(t => t[0], t => t[2]);
+            HashSet<string> answers = new HashSet<string>(ComposeAll(_initial, lookup));
             int answer = answers.Count;
 
             Assert.Equal(518, answer);
@@ -37,9 +37,13 @@ namespace AdventOfCode._2015
         [Fact]
         public void Part2()
         {
-            Dictionary<string, string> inverseTransforms = File
-                .ReadAllLines("Inputs/Day19.txt")
-                .SkipLast(2)
+            // I was able to get the right answer but I don't think I fully solved the problem.
+            // The question asks for min number of steps, and my solution reports the number of steps
+            // for the very first solution it finds.
+            // According to this thread (https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/)
+            // there is only one viable solution based on the nature of the input
+
+            Dictionary<string, string> inverseTransforms = _rawTransforms
                 .Select(s => s.Split(' '))
                 .ToDictionary(t => t[2], t => t[0]);
 
@@ -49,68 +53,40 @@ namespace AdventOfCode._2015
 
         private static IEnumerable<string> ComposeAll(string input, ILookup<string, string> lookup)
         {
+            StringBuilder sb = new StringBuilder(input.Length * 2);
             foreach (var transform in lookup)
             {
-                foreach (string transformed in Compose(input, transform))
-                    yield return transformed;
-            }
-        }
-
-        private static IEnumerable<string> Compose(string input, IGrouping<string, string> transform)
-        {
-            StringBuilder sb = new StringBuilder(input.Length * 2);
-            string key = transform.Key;
-            int index = 0;
-            while (index != -1)
-            {
-                index = input.IndexOf(key, index);
-                if (index != -1)
-                {
-                    int start = index;
-                    index += key.Length;
-                    foreach (string value in transform)
-                    {
-                        sb.Append(input.AsSpan(0, start));
-                        sb.Append(value);
-                        sb.Append(input.AsSpan(index));
-                        yield return sb.ToString();
-                        sb.Clear();
-                    }
-                }
-            }
-        }
-
-        private static IEnumerable<string> DecomposeAll(string input, Dictionary<string, string> transforms)
-        {
-            StringBuilder sb = new StringBuilder(input.Length);
-
-            foreach ((string key, string value) in transforms)
-            {
+                string key = transform.Key;
                 int index = 0;
                 while (index != -1)
                 {
                     index = input.IndexOf(key, index);
                     if (index != -1)
                     {
-                        sb.Append(input.AsSpan(0, index));
-                        sb.Append(value);
+                        int start = index;
                         index += key.Length;
-                        sb.Append(input.AsSpan(index));
-                        yield return sb.ToString();
-                        sb.Clear();
+                        foreach (string value in transform)
+                        {
+                            sb.Append(input.AsSpan(0, start));
+                            sb.Append(value);
+                            sb.Append(input.AsSpan(index));
+                            yield return sb.ToString();
+                            sb.Clear();
+                        }
                     }
                 }
             }
+
         }
 
         private static int Decompose(string input, Dictionary<string, string> transforms, string target)
         {
-            var result = Recurse(input, transforms, target, 0);
+            var result = RecursiveDecompose(input, transforms, target, 0);
             if (!result.found) throw new InvalidOperationException();
             return result.level;
         }
 
-        private static (bool found, int level) Recurse(string input, Dictionary<string, string> transforms, string target, int level)
+        private static (bool found, int level) RecursiveDecompose(string input, Dictionary<string, string> transforms, string target, int level)
         {
             if (input == target)
             {
@@ -130,7 +106,7 @@ namespace AdventOfCode._2015
                         sb.Append(kvp.Value);
                         index += kvp.Key.Length;
                         sb.Append(input.AsSpan(index));
-                        var result = Recurse(sb.ToString(), transforms, target, level + 1);
+                        var result = RecursiveDecompose(sb.ToString(), transforms, target, level + 1);
                         if (result.found)
                             return result; 
                         sb.Clear();
---
+++ AdventOfCode.2015/Day20.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace AdventOfCode._2015
+{
+    public class Day20
+    {
+        const int Input = 36_000_000;
+
+        [Fact]
+        public void Part1()
+        {
+            int target = Input / 10;
+
+            int[] bigArray = new int[target + 1];
+            int i = 1;
+            for (; i <= target; i++)
+            {
+                int index = i;
+                while (index <= target)
+                {
+                    bigArray[index] += i;
+                    index += i;
+                }
+
+                if (bigArray[i] >= target)
+                    break;
+            }
+
+            Assert.Equal(831_600, i);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            int target = Input / 11;
+
+            int[] bigArray = new int[target + 1];
+            int i = 1;
+            for (; i <= target; i++)
+            {
+                int index = i;
+                for (int j = 0; j < 50 && index <= target; j++)
+                {
+                    bigArray[index] += i;
+                    index += i;
+                }
+
+                if (bigArray[i] >= target)
+                    break;
+            }
+
+            Assert.Equal(884_520, i);
+        }
+    }
+}
