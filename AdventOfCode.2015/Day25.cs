c4dd63f996cf0f82d323020f4ac4d06395e8dfac
Chuck Ries <chuckr@microsoft.com>
2020-10-28 16:19:14 -0700

    finished 2015

---
+++ AdventOfCode.2015/Day25.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+using Xunit;
+
+namespace AdventOfCode._2015
+{
+    public class Day25
+    {
+        const int Row = 2947;
+        const int Col = 3029;
+
+        [Fact]
+        public void Part1()
+        {
+            int index = GetIndex(Col, Row);
+            long code = 20151125;
+            for (int i = 0; i < index - 1; i++)
+            {
+                code = (code * 252533) % 33554393;
+            }
+
+            Assert.Equal(19980801, code);
+        }
+
+        private int GetIndex(int x, int y)
+        {
+            int diagNumber = x + y - 1;
+            int diagTotal = (((diagNumber - 1) * diagNumber) / 2) + x;
+            return diagTotal;
+        }
+    }
+}
