4701558b86c79370fb4cb43274688835aa5bb38b
Chuck Ries <chuckr@microsoft.com>
2021-11-30 11:01:05 -0800

    2021 skeleton

---
+++ AdventOfCode.2021/Day01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode._2021
+{
+    public class Day01
+    {
+        [Fact]
+        public void Part1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            Assert.True(true);
+        }
+
+    }
+}
