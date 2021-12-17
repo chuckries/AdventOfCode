4756b3b568eab220fd88d3dd4540f3eb3e50fe82
Chuck Ries <chuckr@microsoft.com>
2021-12-16 16:52:10 -0800

    day 16 different

--- AdventOfCode.2021/Day16.cs
+++ AdventOfCode.2021/Day16.cs
@@ -1,242 +1,157 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace AdventOfCode._2021
+﻿namespace AdventOfCode._2021
 {
     public class Day16
     {
-        private record Packet(
-            int BitCount,
-            int Version,
-            int Type,
-            int LengthTypeId,
-            int Count,
-            long Literal);
-
-        private class BitReader
+        private class Packet
         {
-            readonly int[] _packet;
-            int _packetIdx;
-            int _bitIdx;
+            public readonly int Version;
+            public readonly int Type;
+            public readonly long Literal;
 
-            public BitReader(string packet)
+            private readonly List<Packet> _subPackets;
+
+            private class PacketReader
             {
-                _packet = packet.Select(c => CharToByte(c)).ToArray();
-                _packetIdx = 0;
-                _bitIdx = 3;
-            }
+                readonly int[] _data;
+                int _dataIdx;
+                int _bitIdx;
+                int _position;
 
-            public bool TryReadBits(int bits, out int result)
-            {
-                result = 0;
-
-                if (_packetIdx >= _packet.Length)
-                    return false;
-
-                if (bits > 32)
-                    throw new InvalidOperationException();
-
-                while (bits-- > 0)
+                public PacketReader(string input)
                 {
-                    result = (result << 1) | (_packet[_packetIdx] & (1 << _bitIdx)) >> (_bitIdx);
-                    _bitIdx--;
-                    if (_bitIdx < 0)
-                    {
-                        _packetIdx++;
-                        _bitIdx = 3;
-
-                        if (_packetIdx >= _packet.Length && bits >= 0)
-                            return false;
-                    }
+                    _data = input.Select(c => c <= '9' ? c - '0' : c - 'A' + 10).ToArray();
+                    _dataIdx = 0;
+                    _bitIdx = 3;
+                    _position = 0;
                 }
 
-                return true;
-            }
-
-            public bool TryReadPacket([NotNullWhen(true)] out Packet? packet)
-            {
-                packet = null;
-
-                if (!TryReadBits(3, out int version))
-                    return false;
-
-                if (!TryReadBits(3, out int type))
-                    return false;
-
-                int bitCount = 6;
-                int lengthTypeId = 0;
-                long literal = 0;
-                int count = 0;
-
-                if (type == 4)
+                public Packet ReadPackets()
                 {
-                    if (!TryReadLiteral(out literal, out int literalLength))
-                        return false;
-                    bitCount += literalLength;
-                }
-                else
-                {
-                    if (!TryReadBits(1, out lengthTypeId))
-                        return false;
+                    int version = ReadBits(3);
+                    int type = ReadBits(3);
 
-                    if (lengthTypeId == 0)
+                    Packet packet;
+                    if (type is 4)
                     {
-                        if (!TryReadBits(15, out count))
-                            return false;
-                        bitCount += 16;
+                        long literal = ReadLiteral();
+                        packet = new Packet(version, type, literal);
                     }
                     else
                     {
-                        if (!TryReadBits(11, out count))
-                            return false;
-                        bitCount += 12;
+                        packet = new Packet(version, type, 0);
+                        ReadSubPackets(packet);
+                    }
+
+                    return packet;
+                }
+
+                private void ReadSubPackets(Packet packets)
+                {
+                    int lengthTypeId = ReadBits(1);
+                    if (lengthTypeId is 0)
+                    {
+                        int bitCount = ReadBits(15);
+                        int offset = _position + bitCount;
+                        while (_position < offset)
+                            packets._subPackets.Add(ReadPackets());
+                    }
+                    else
+                    {
+                        int subPackets = ReadBits(11);
+                        for (int i = 0; i < subPackets; i++)
+                            packets._subPackets.Add(ReadPackets());
                     }
                 }
 
-                packet = new Packet(bitCount, version, type, lengthTypeId, count, literal);
-                return true;
-            }
-
-            public bool TryReadLiteral(out long literal, out int bitLength)
-            {
-                literal = 0;
-                bitLength = 0;
-
-                while (true)
+                private long ReadLiteral()
                 {
-                    if (!TryReadBits(5, out int next))
-                        return false;
+                    long literal = 0;
 
-                    bitLength += 5;
-                    if (bitLength > 80)
-                        throw new InvalidOperationException();
+                    while (true)
+                    {
+                        int next = ReadBits(5);
+                        literal = (literal << 4) | (long)(next & 0x0F);
+                        if ((next & 0x10) == 0)
+                            break;
+                    }
 
-                    literal = (literal << 4) | (long)(next & 0x0F);
-
-                    if ((next & 0x10) == 0)
-                        break;
+                    return literal;
                 }
 
-                return true;
+                private int ReadBits(int bits)
+                {
+                    if (bits > 32)
+                        throw new InvalidOperationException();
+
+                    int result = 0;
+                    for (int i = 0; i < bits; i++)
+                    {
+                        result = (result << 1) | (_data[_dataIdx] & (1 << _bitIdx)) >> (_bitIdx);
+                        _bitIdx--;
+                        if (_bitIdx < 0)
+                        {
+                            _dataIdx++;
+                            _bitIdx = 3;
+                        }
+                    }
+
+                    _position += bits;
+                    return result;
+                }
             }
 
-            private static int CharToByte(char c) =>
-                (byte)(c <= '9' ? c - '0' : c - 'A' + 10);
+            private Packet(int version, int type, long literal)
+            {
+                Version = version;
+                Type = type;
+                Literal = literal;
+                _subPackets = new();
+            }
+
+            public static Packet ParsePacketTree(string input)
+            {
+                PacketReader reader = new PacketReader(input);
+                return reader.ReadPackets();
+            }
+
+            public int SumVersions()
+            {
+                return Version + _subPackets.Sum(p => p.SumVersions());
+            }
+
+            public long Evaluate() => Type switch
+            {
+                0 => _subPackets.Sum(p => p.Evaluate()),
+                1 => _subPackets.Aggregate(1L, (a, b) => a * b.Evaluate()),
+                2 => _subPackets.Min(p => p.Evaluate()),
+                3 => _subPackets.Max(p => p.Evaluate()),
+                4 => Literal,
+                5 => _subPackets[0].Evaluate() > _subPackets[1].Evaluate() ? 1 : 0,
+                6 => _subPackets[0].Evaluate() < _subPackets[1].Evaluate() ? 1 : 0,
+                7 => _subPackets[0].Evaluate() == _subPackets[1].Evaluate() ? 1 : 0,
+                _ => throw new InvalidOperationException()
+            };
         }
 
-        BitReader _reader;
+        Packet _root;
 
         public Day16()
         {
-            _reader = new BitReader(File.ReadAllText("Inputs/Day16.txt"));
+            _root = Packet.ParsePacketTree(File.ReadAllText("Inputs/Day16.txt"));
         }
 
         [Fact]
         public void Part1()
         {
-            int answer = 0;
-            while (_reader.TryReadPacket(out Packet? packet))
-                answer += packet.Version;
-
+            int answer = _root.SumVersions();
             Assert.Equal(893, answer);
         }
 
         [Fact]
         public void Part2()
         {
-            (long answer, _) = EvalNextPacket();
+            long answer = _root.Evaluate();
             Assert.Equal(4358595186090, answer);
-        }
-
-        private delegate bool tryEvalNextPacket(out long result);
-
-        private (long result, int bitCount) EvalNextPacket()
-        {
-            if (!_reader.TryReadPacket(out Packet? packet))
-                throw new InvalidOperationException();
-
-            if (packet.Type is 4)
-                return (packet.Literal, packet.BitCount);
-            else
-            {
-                int totalBitCount = 0;
-                int immediateSubs = 0;
-                int max = packet.Count;
-
-                bool CheckBitCount() => totalBitCount >= max;
-                bool CheckImmediateSubCount() => immediateSubs >= max;
-
-                Func<bool> check = packet.LengthTypeId is 0 ?
-                    CheckBitCount :
-                    CheckImmediateSubCount;
-
-                bool TryEvalNext(out long evalResult)
-                {
-                    evalResult = 0;
-                    if (check())
-                        return false;
-
-                    (evalResult, int bitCount) = EvalNextPacket();
-                    immediateSubs++;
-                    totalBitCount += bitCount;
-                    return true;
-                }
-
-                long result = 0;
-                long evalResult;
-                long a;
-                long b;
-
-                switch (packet.Type)
-                {
-                    case 0:
-                        while (TryEvalNext(out evalResult))
-                            result += evalResult;
-                        break;
-
-                    case 1:
-                        result = 1;
-                        while (TryEvalNext(out evalResult))
-                            result *= evalResult;
-                        break;
-
-                    case 2:
-                        TryEvalNext(out result);
-                        while (TryEvalNext(out evalResult))
-                            if (evalResult < result)
-                                result = evalResult;
-                        break;
-
-                    case 3:
-                        TryEvalNext(out result);
-                        while (TryEvalNext(out evalResult))
-                            if (evalResult > result)
-                                result = evalResult;
-                        break;
-
-                    case 5:
-                        TryEvalNext(out a);
-                        TryEvalNext(out b);
-                        result = a > b ? 1 : 0;
-                        break;
-
-                    case 6:
-                        TryEvalNext(out a);
-                        TryEvalNext(out b);
-                        result = a < b ? 1 : 0;
-                        break;
-
-                    case 7:
-                        TryEvalNext(out a);
-                        TryEvalNext(out b);
-                        result = a == b ? 1 : 0;
-                        break;
-
-                    default:
-                        throw new InvalidOperationException();
-                }
-
-                return (result, totalBitCount + packet.BitCount);
-            }
         }
     }
 }
