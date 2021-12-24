e24ac6d1bda5fb5a6cfa65df02fff8bb5c85bbd8
Chuck Ries <chuckr@microsoft.com>
2021-12-24 15:43:23 -0600

    day 22 refactor

--- AdventOfCode.2021/Day22.cs
+++ AdventOfCode.2021/Day22.cs
@@ -29,158 +29,94 @@ namespace AdventOfCode._2021
         [Fact]
         public void Part1()
         {
-            List<Region> space = _input.Take(1).ToList();
+            long answer = _input
+                .Where(r => r.Low.X >= -50 && r.Low.Y >= -50 && r.Low.Z >= -50 && r.High.X <= 50 && r.High.Y <= 50 && r.High.Z <= 50)
+                .Aggregate(new List<Region>(), (s, r) => AddRegionToSpace(s, r))
+                .Where(r => r.Status)
+                .Sum(r => r.Volume);
 
-            foreach (var instr in _input.Skip(1))
-            {
-                if (instr.High.X < -50 || instr.Low.X > 50 ||
-                    instr.High.Y < -50 || instr.Low.Y > 50 ||
-                    instr.High.Z < -50 || instr.Low.Z > 50)
-                {
-                    continue;
-                }
-
-                space = AddRegionToSpace(space, instr);
-            }
-
-            long answer = space.Where(r => r.Status).Aggregate(0L, (tot, r) => tot += r.Volume);
             Assert.Equal(588120, answer);
         }
 
         [Fact]
         public void Part2()
         {
-            List<Region> space = _input.Take(1).ToList();
-            foreach (var instr in _input.Skip(1))
-            {
-                space = AddRegionToSpace(space, instr);
-            }
+            long answer = _input
+                .Aggregate(new List<Region>(), (s, r) => AddRegionToSpace(s, r))
+                .Where(r => r.Status)
+                .Sum(r => r.Volume);
 
-            long answer = space.Where(r => r.Status).Aggregate(0L, (tot, r) => tot += r.Volume);
             Assert.Equal(1134088247046731, answer);
         }
 
-        private List<Region> AddRegionToSpace(List<Region> space, Region incoming)
+        private List<Region> AddRegionToSpace(List<Region> space, Region newRegion)
         {
-            List<Region> newSpace = new(space.Count * 2);
-            List<Region> toIntersect = new() { incoming };
-            foreach (Region existing in space)
-            {
-                List<Region> nextToIntersect = new(toIntersect.Count);
-                foreach(Region intersectWith in toIntersect)
-                {
-                    (var olds, var news) = IntersectRegions(existing, intersectWith);
-                    newSpace.AddRange(olds);
-                    nextToIntersect.AddRange(news);
-                }
-                toIntersect = nextToIntersect;
-            }
+            List<Region> newSpace = new(space.Count * 6 + 1);
 
-            newSpace.AddRange(toIntersect);
+            foreach(Region existing in space)
+                IntersectRegions(existing, newRegion, newSpace);
+
+            newSpace.Add(newRegion);
             return newSpace;
         }
 
-        private (List<Region> olds, List<Region> news) IntersectRegions(Region existing, Region incoming)
+        private void IntersectRegions(Region existing, Region incoming, List<Region> space)
         {
-            List<Region> olds = new();
-            List<Region> news = new();
-
             if (incoming.Low.X > existing.High.X || incoming.High.X < existing.Low.X ||
                 incoming.Low.Y > existing.High.Y || incoming.High.Y < existing.Low.Y ||
                 incoming.Low.Z > existing.High.Z || incoming.High.Z < existing.Low.Z)
             {
-                // no intersection
-                olds.Add(existing);
-                news.Add(incoming);
-                return (olds, news);
+                space.Add(existing);
+                return;
             }
 
             if (incoming.Low.X <= existing.Low.X && incoming.High.X >= existing.High.X &&
                 incoming.Low.Y <= existing.Low.Y && incoming.High.Y >= existing.High.Y &&
                 incoming.Low.Z <= existing.Low.Z && incoming.High.Z >= existing.High.Z)
             {
-                //existing is fully consumed by incoming
-                news.Add(incoming);
-                return (olds, news);
+                return;
             }
-
-            news.Add(incoming);
 
             // -X
             if (existing.Low.X < incoming.Low.X)
             {
-                olds.Add(new Region(existing.Status, existing.Low, new(incoming.Low.X - 1, existing.High.Y, existing.High.Z)));
+                space.Add(new Region(existing.Status, existing.Low, new(incoming.Low.X - 1, existing.High.Y, existing.High.Z)));
                 existing = new Region(existing.Status, new(incoming.Low.X, existing.Low.Y, existing.Low.Z), existing.High);
             }
-            //else if (incoming.Low.X < existing.Low.X)
-            //{
-            //    news.Add(new Region(incoming.Status, incoming.Low, new(existing.Low.X - 1, incoming.High.Y, incoming.High.Z)));
-            //    incoming = new Region(incoming.Status, new(existing.Low.X, incoming.Low.Y, incoming.Low.Z), incoming.High);
-            //}
 
             // +X
             if (existing.High.X > incoming.High.X)
             {
-                olds.Add(new Region(existing.Status, new(incoming.High.X + 1, existing.Low.Y, existing.Low.Z), existing.High));
+                space.Add(new Region(existing.Status, new(incoming.High.X + 1, existing.Low.Y, existing.Low.Z), existing.High));
                 existing = new Region(existing.Status, existing.Low, new(incoming.High.X, existing.High.Y, existing.High.Z));
             }
-            //else if (incoming.High.X > existing.High.X)
-            //{
-            //    news.Add(new Region(incoming.Status, new(existing.High.X + 1, incoming.Low.Y, incoming.Low.Z), incoming.High));
-            //    incoming = new Region(incoming.Status, incoming.Low, new(existing.High.X, incoming.High.Y, incoming.High.Z));
-            //}
 
             // -Y
             if (existing.Low.Y < incoming.Low.Y)
             {
-                olds.Add(new Region(existing.Status, existing.Low, new(existing.High.X, incoming.Low.Y - 1, existing.High.Z)));
+                space.Add(new Region(existing.Status, existing.Low, new(existing.High.X, incoming.Low.Y - 1, existing.High.Z)));
                 existing = new Region(existing.Status, new(existing.Low.X, incoming.Low.Y, existing.Low.Z), existing.High);
             }
-            //else if (incoming.Low.Y < existing.Low.Y)
-            //{
-            //    news.Add(new Region(incoming.Status, incoming.Low, new(incoming.High.X, existing.Low.Y - 1, incoming.High.Z)));
-            //    incoming = new Region(incoming.Status, new(incoming.Low.X, existing.Low.Y, incoming.Low.Z), incoming.High);
-            //}
 
             // +Y
             if (existing.High.Y > incoming.High.Y)
             {
-                olds.Add(new Region(existing.Status, new(existing.Low.X, incoming.High.Y + 1, existing.Low.Z), existing.High));
+                space.Add(new Region(existing.Status, new(existing.Low.X, incoming.High.Y + 1, existing.Low.Z), existing.High));
                 existing = new Region(existing.Status, existing.Low, new(existing.High.X, incoming.High.Y, existing.High.Z));
             }
-            //else if (incoming.High.Y > existing.High.Y)
-            //{
-            //    news.Add(new Region(incoming.Status, new(incoming.Low.X, existing.High.Y + 1, incoming.Low.Z), incoming.High));
-            //    incoming = new Region(incoming.Status, incoming.Low, new(incoming.High.X, existing.High.Y, incoming.High.Z));
-            //}
 
             // -Z
             if (existing.Low.Z < incoming.Low.Z)
             {
-                olds.Add(new Region(existing.Status, existing.Low, new(existing.High.X, existing.High.Y, incoming.Low.Z - 1)));
+                space.Add(new Region(existing.Status, existing.Low, new(existing.High.X, existing.High.Y, incoming.Low.Z - 1)));
                 existing = new Region(existing.Status, new(existing.Low.X, existing.Low.Y, incoming.Low.Z), existing.High);
             }
-            //else if (incoming.Low.Z < existing.Low.Z)
-            //{
-            //    news.Add(new Region(incoming.Status, incoming.Low, new(incoming.High.X, incoming.High.Y, existing.Low.Z - 1)));
-            //    incoming = new Region(incoming.Status, new(incoming.Low.X, incoming.Low.Y, existing.Low.Z), incoming.High);
-            //}
 
             // +Z
             if (existing.High.Z > incoming.High.Z)
             {
-                olds.Add(new Region(existing.Status, new(existing.Low.X, existing.Low.Y, incoming.High.Z + 1), existing.High));
-                existing = new Region(existing.Status, existing.Low, new(existing.High.X, existing.High.Y, incoming.High.Z));
+                space.Add(new Region(existing.Status, new(existing.Low.X, existing.Low.Y, incoming.High.Z + 1), existing.High));
             }
-            //else if (incoming.High.Z > existing.High.Z)
-            //{
-            //    news.Add(new Region(incoming.Status, new(incoming.Low.X, incoming.Low.Y, existing.High.Z + 1), incoming.High));
-            //    incoming = new Region(incoming.Status, incoming.Low, new(incoming.High.X, incoming.High.Y, existing.High.Z));
-            //}
-
-            //olds.Add(incoming);
-
-            return (olds, news);
         }
     }
 }
